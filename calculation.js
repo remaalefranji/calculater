f1b47f1d9c04b12f3027ad58b43cec729bf9b491
Rema alefranji <67292104+remaalefranji@users.noreply.github.com>
2021-03-28 15:14:57 +0300

    Add files via upload

---
+++ calculation.js
@@ -0,0 +1,74 @@
+var exp ='', number, deimal , equal, operator , allowSR = true;
+var textview = document.forms['myform']['textview'];
+
+function insertnum(num){
+	if (equal) {
+		exp = num;
+		textview.value = exp;
+		number = true;
+		equal = false;
+
+	}
+	else{
+		exp = textview.value + num;
+		textview.value = exp;
+		number = true;
+	}
+	if (operator) deimal = false;
+	SR('a');
+	
+}
+function insertOp(op){
+	textview.value = exp + op;
+	operator = true;
+	equal = false;
+	allowSR = false;
+	SR('a');
+	
+}
+function insertDec(){
+	if(number && !decimal) {
+		textview.value = exp + '.';
+		decimal = true;
+		operator = false;
+	}
+}
+
+function equalto(){
+	if(exp) {
+		exp = eval(exp);
+		textview.value = exp;
+		equal = true;
+		decimal = false;
+		number = false;
+		allowSR = true;
+		SR('a');
+	}
+}
+function clean(){
+	exp = '';
+	textview.value = exp;
+	decimal = false;
+}
+function back(){
+	exp = textview.value;
+	exp = exp.substring(0,exp.length-1);
+	textview.value = exp;
+}
+function SR(x) {
+	if(x=='r'){
+		exp=math.sqrt(exp);
+		textview.value=exp;
+	}
+	else if (x=='s'){
+		exp = exp*exp;
+		textview.value=exp;
+	}
+	else if (x=='a'&& allowSR){
+		document.getElementById('r').disabled=false;
+		document.getElementById('s').disabled=false;
+	}
+	else if(!allowSR){
+		document.getElementById('r').disabled=true;
+		document.getElementById('s').disabled=true;
+	}
